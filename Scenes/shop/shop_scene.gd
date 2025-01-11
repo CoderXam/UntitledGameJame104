8d10ce6d87a97202af419081ae4763e84f2f3bb1
CoderXam <maxdaniellevine@gmail.com>
2025-01-11 18:51:26 -0500

    randomized shop items

--- Scenes/shop/shop_scene.gd
+++ Scenes/shop/shop_scene.gd
@@ -12,8 +12,36 @@ var shop_inv = {"Fire": ["Fireball","Launch a scorching fireball attack", 5, 10]
 				"Amplify" : ["Amplify","Amplify other runes", 5, 10], 
 				"Thorn" : ["Thorn Needles","Riddle your enemies with thorny needles", 5, 10]}
 # Called when the node enters the scene tree for the first time.
+
+var available = []
+var randnum = 0
+
 func _ready() -> void:
-	print(shop_inv["Fire"][1])
+	for i in 4:
+		randnum = randi_range(1,100)
+		if randnum > 0 and randnum < 11:
+			available.append(shop_inv["Fire"])
+		elif randnum > 10 and randnum < 21:
+			available.append(shop_inv["Magma"])
+		elif randnum > 20 and randnum < 31:
+			available.append(shop_inv["Water"])
+		elif randnum > 30 and randnum < 41:
+			available.append(shop_inv["Shield"])
+		elif randnum > 40 and randnum < 51:
+			available.append(shop_inv["Orb"])
+		elif randnum > 50 and randnum < 61:
+			available.append(shop_inv["Lifesteal"])
+		elif randnum > 60 and randnum < 71:
+			available.append(shop_inv["Root"])
+		elif randnum > 70 and randnum < 81:
+			available.append(shop_inv["Lightning"])
+		elif randnum > 80 and randnum < 91:
+			available.append(shop_inv["Amplify"])
+		elif randnum > 90 and randnum < 101:
+			available.append(shop_inv["Thorn"])
+		else:
+			print("how")
+	print(available)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,19 +50,19 @@ func _process(delta: float) -> void:
 	
 
 func _on_rune_1_pressed() -> void:
-	print("pruchased " + shop_inv["Fire"][0])
+	print("pruchased " + available[0][0])
 	pass # Replace with function body.
 
 func _on_rune_2_pressed() -> void:
-	print("pruchased " + shop_inv["Magma"][0])
+	print("pruchased " + available[1][0])
 	pass # Replace with function body.
 
 
 func _on_rune_3_pressed() -> void:
-	print("pruchased " + shop_inv["Water"][0])
+	print("pruchased " + available[2][0])
 	pass # Replace with function body.
 
 
 func _on_rune_4_pressed() -> void:
-	print("pruchased " + shop_inv["Shield"][0])
+	print("pruchased " + available[3][0])
 	pass # Replace with function body.
