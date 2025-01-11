555cf872be2021c9ca521fefd1371cca68d00b8a
BaconGod512 <hazanandrew@gmail.com>
2025-01-11 11:59:50 -0500

    created shop dictionary with descriptions

---
+++ Scenes/shop/shop_scene.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var shop_inv = {"Fire": "Launch a scorching fireball attack",
+ 				"Magma" : "Turn the ground to molten rock",
+			 	"Water" : "Blast foes back and deal damage with a pressurized beam of water",
+				"Shield" : "Block incoming attacks with an arcane ward",
+			 	"Orb" : "Pierce through enemies with a spectral orb", 
+				"Lifesteal" : "Sap away the lifeforce of enemies for yourself", 
+				"Root" : "Fix enemies in place with grasping roots", 
+				"Lightning" : "Shock and stun enemies with a bolt of lightning", 
+				"Amplify" : "Amplify other runes", 
+				"Thorn" : "Riddle your enemies with thorny needles"}
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(shop_inv["Fire"])
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
