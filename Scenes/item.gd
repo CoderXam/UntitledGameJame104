714dad899edbb36cb74a955d79283885d3e6486f
CoderXam <maxdaniellevine@gmail.com>
2025-01-10 20:50:25 -0500

    created item

---
+++ Scenes/item.gd
@@ -0,0 +1,8 @@
+class_name Item
+
+extends Node
+
+@export var item_name : String
+
+func use():
+	print("used" + item_name)
--- Scenes/player_data.gd
+++ Scenes/player_data.gd
@@ -12,8 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-	
-var inventory = []
+
+var inventory: Array = []
+@export var starting_item: Item
+
 func add_to_inventory(item) -> void:
 	inventory.append(item)
