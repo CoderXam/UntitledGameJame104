d2d12cf77579fad97ec127953b516eb56bfa7c61
CoderXam <maxdaniellevine@gmail.com>
2025-01-12 00:31:44 -0500

    rune rework

---
+++ Scenes/Rune.gd
@@ -0,0 +1,9 @@
+class_name Rune
+
+var name: String
+var description: String
+var price: int
+var shop_chance: int
+
+func use():
+	print("used "+ name)
--- player_data.gd
+++ player_data.gd
@@ -3,23 +3,27 @@ extends Node
 @export var max_health = 10
 @export var player_health = 10
 
+@export var currency = 21
+
+var inventory: Array = []
+var starting_rune: Rune = Rune.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Hello Player")
+	starting_rune.name = "magic thing"
+	starting_rune.description = "a magical thing"
+	add_to_inventory(starting_rune)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
-@export var currency = 21
-
-var inventory: Array = []
-@export var starting_item: Item
 
 func add_to_inventory(item) -> void:
 	inventory.append(item)
-	
+
 func _input(event):
 	if event.is_action_pressed("use"):
-		starting_item.use()
+		starting_rune.use()
