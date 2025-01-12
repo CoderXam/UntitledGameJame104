06d95f88b5cd8d81d32c30037dc7c48e4d455496
CoderXam <maxdaniellevine@gmail.com>
2025-01-11 22:51:46 -0500

    shop stock works

--- Scenes/shop/shop_scene.gd
+++ Scenes/shop/shop_scene.gd
@@ -1,16 +1,17 @@
 extends Node2D
 	
-#0 pos in array = name, 1 = description, 2 = price, 3 = probability of showing up
-var shop_inv = {"Fire": ["Fireball","Launch a scorching fireball attack", 5, 10],
- 				"Magma" : ["Magma","Turn the ground to molten rock", 5, 10],
-			 	"Water" : ["Water Blast","Blast foes back and deal damage with a pressurized beam of water", 5, 10],
-				"Shield" : ["Shield","Block incoming attacks with an arcane ward", 5, 10],
-			 	"Orb" : ["Orb","Pierce through enemies with a spectral orb", 5, 10],
-				"Lifesteal" : ["Lifesteal","Sap away the lifeforce of enemies for yourself", 5, 10], 
-				"Root" : ["Root Snatch","Fix enemies in place with grasping roots", 5, 10],
-				"Lightning" : ["Lightning Bolt","Shock and stun enemies with a bolt of lightning", 5, 10], 
-				"Amplify" : ["Amplify","Amplify other runes", 5, 10], 
-				"Thorn" : ["Thorn Needles","Riddle your enemies with thorny needles", 5, 10]}
+# positions in array:
+# 0 = name, 1 = description, 2 = price, 3 = probability of showing up in shop, 4 = amount in stock
+var shop_inv = {"Fire": ["Fireball","Launch a scorching fireball attack", 5, 10, 2],
+ 				"Magma" : ["Magma","Turn the ground to molten rock", 5, 10, 2],
+			 	"Water" : ["Water Blast","Blast foes back and deal damage with a pressurized beam of water", 5, 10, 2],
+				"Shield" : ["Shield","Block incoming attacks with an arcane ward", 5, 10, 2],
+			 	"Orb" : ["Orb","Pierce through enemies with a spectral orb", 5, 10, 2],
+				"Lifesteal" : ["Lifesteal","Sap away the lifeforce of enemies for yourself", 5, 10, 2], 
+				"Root" : ["Root Snatch","Fix enemies in place with grasping roots", 5, 10, 2],
+				"Lightning" : ["Lightning Bolt","Shock and stun enemies with a bolt of lightning", 5, 10, 2], 
+				"Amplify" : ["Amplify","Amplify other runes", 5, 10, 2], 
+				"Thorn" : ["Thorn Needles","Riddle your enemies with thorny needles", 5, 10, 2]}
 # Called when the node enters the scene tree for the first time.
 
 var pool = [shop_inv["Fire"],shop_inv["Magma"],shop_inv["Water"],shop_inv["Shield"],shop_inv["Orb"],shop_inv["Lifesteal"],shop_inv["Root"],shop_inv["Lightning"],shop_inv["Amplify"],shop_inv["Thorn"],]
@@ -37,7 +38,7 @@ func _ready() -> void:
 		print(str(i+1)+". "+available[i][0])
 		names[i].text = available[i][0]
 		desc[i].text = available[i][1]
-		stock[i].text = "1 in stock"
+		stock[i].text = str(available[i][4]) +" in stock"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -47,54 +48,70 @@ func _process(delta: float) -> void:
 
 func _on_rune_1_pressed() -> void:
 	var rune = available[0]
-	if PlayerData1.currency >= rune[2]:
+	if PlayerData1.currency >= rune[2] and rune[4] > 0:
 		PlayerData1.currency -= rune[2]
 		PlayerData1.add_to_inventory(rune)
-		stock[0].text = "out of stock"
+		rune[4] -= 1
+		if rune[4] > 0:
+			stock[0].text = str(rune[4]) +" in stock"
+		else:
+			stock[0].text = "out of stock"
 		
 		print("pruchased " + rune[0])
 		print("currency: "+ str(PlayerData1.currency))
 		print(PlayerData1.inventory)
 	else:
-		print("not enough money")
+		print("not enough money or out of stock")
 
 func _on_rune_2_pressed() -> void:
 	var rune = available[1]
-	if PlayerData1.currency >= rune[2]:
+	if PlayerData1.currency >= rune[2] and rune[4] > 0:
 		PlayerData1.currency -= rune[2]
 		PlayerData1.add_to_inventory(rune)
-		stock[0].text = "out of stock"
+		rune[4] -= 1
+		if rune[4] > 0:
+			stock[1].text = str(rune[4]) +" in stock"
+		else:
+			stock[1].text = "out of stock"
 		
 		print("pruchased " + rune[0])
 		print("currency: "+ str(PlayerData1.currency))
 		print(PlayerData1.inventory)
 	else:
-		print("not enough money")
+		print("not enough money or out of stock")
 
 
 func _on_rune_3_pressed() -> void:
 	var rune = available[2]
-	if PlayerData1.currency >= rune[2]:
+	if PlayerData1.currency >= rune[2] and rune[4] > 0:
 		PlayerData1.currency -= rune[2]
 		PlayerData1.add_to_inventory(rune)
-		stock[0].text = "out of stock"
+		rune[4] -= 1
+		if rune[4] > 0:
+			stock[2].text = str(rune[4]) +" in stock"
+		else:
+			stock[2].text = "out of stock"
 		
 		print("pruchased " + rune[0])
 		print("currency: "+ str(PlayerData1.currency))
 		print(PlayerData1.inventory)
 	else:
-		print("not enough money")
+		print("not enough money or out of stock")
 
 
 func _on_rune_4_pressed() -> void:
 	var rune = available[3]
-	if PlayerData1.currency >= rune[2]:
+	if PlayerData1.currency >= rune[2] and rune[4] > 0:
 		PlayerData1.currency -= rune[2]
 		PlayerData1.add_to_inventory(rune)
-		stock[0].text = "out of stock"
+		rune[4] -= 1
+		if rune[4] > 0:
+			stock[3].text = str(rune[4]) +" in stock"
+		else:
+			stock[3].text = "out of stock"
 		
 		print("pruchased " + rune[0])
 		print("currency: "+ str(PlayerData1.currency))
 		print(PlayerData1.inventory)
 	else:
-		print("not enough money")
+		print("not enough money or out of stock")
