252bb2a0bf6f97e761e2a649e78e8a9f420b5ce1
CoderXam <maxdaniellevine@gmail.com>
2025-01-18 23:26:16 -0500

    one single comment

--- Scenes/Combat Stuff/combat_scene.gd
+++ Scenes/Combat Stuff/combat_scene.gd
@@ -54,7 +54,9 @@ func _process(delta):
 func refresh_pool() -> void:
 	# Generate rune pool
 	#for i in PlayerData1.rune_pool: print("leftover pool: ",i.rune_name)
-	for i in 4-len(PlayerData1.rune_pool):
+	
+	#        v number of runes that will be in the rune pool
+	for i in 9-len(PlayerData1.rune_pool):
 		PlayerData1.rune_pool.append(PlayerData1.inventory[randi_range(0,PlayerData1.inventory.size()-1)])
 		print("added ",PlayerData1.rune_pool.back().rune_name)
 	#for i in PlayerData1.rune_pool: print("new pool: ",i.rune_name)
